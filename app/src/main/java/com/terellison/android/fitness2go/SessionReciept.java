10f00ffb8e379f7f983d6494a33c888863f3451d
Terry Ellison <ellisonterry4@gmail.com>
2017-04-12 14:02:53 -0400

    added blank Activities for planned functionality

---
+++ app/src/main/java/com/terellison/android/fitness2go/SessionReciept.java
@@ -0,0 +1,13 @@
+package com.terellison.android.fitness2go;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SessionReciept extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session_reciept);
+    }
+}
---
+++ app/src/main/java/com/terellison/android/fitness2go/SessionsList.java
@@ -0,0 +1,13 @@
+package com.terellison.android.fitness2go;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SessionsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sessions_list);
+    }
+}
