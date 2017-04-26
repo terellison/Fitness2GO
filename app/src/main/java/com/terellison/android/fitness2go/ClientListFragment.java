5e0f7e7bb87c6bbf27d7c77527a34f8001d33b6a
Terry Ellison <ellisonterry4@gmail.com>
2017-04-26 19:58:35 -0400

    added Adapter to updateUI() in ClientListFragment added RecyclerView to fragment_client_detail created list_item_client.xml

--- app/src/main/java/com/terellison/android/fitness2go/ClientListFragment.java
+++ app/src/main/java/com/terellison/android/fitness2go/ClientListFragment.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by Student on 4/26/2017.
@@ -15,6 +18,7 @@ import android.view.ViewGroup;
 public class ClientListFragment extends Fragment {
 
     private RecyclerView mClientRecyclerView;
+    private ClientAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +28,56 @@ public class ClientListFragment extends Fragment {
         mClientRecyclerView = (RecyclerView) view.findViewById(R.id.client_recycler_view);
         mClientRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUI();
+
         return view;
+    }
+
+    private void updateUI() {
+        ClientSingleton clientSingleton = ClientSingleton.get(getActivity());
+        List<Client> clients = clientSingleton.getClients();
+
+        mAdapter = new ClientAdapter(clients);
+        mClientRecyclerView.setAdapter(mAdapter);
+    }
+
+    public class ClientHolder extends RecyclerView.ViewHolder {
+        public TextView mClientNameTextView;
+
+        public ClientHolder(View itemView) {
+            super(itemView);
+
+            mClientNameTextView = (TextView) itemView;
+        }
+
+    }
+
+    private class ClientAdapter extends RecyclerView.Adapter<ClientHolder> {
+
+        private List<Client> mClients;
+
+        public ClientAdapter(List<Client> clients) {
+            mClients = clients;
+        }
+
+
+        @Override
+        public ClientHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new ClientHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ClientHolder holder, int position) {
+            Client client = mClients.get(position);
+            holder.mClientNameTextView.setText(client.getClientName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mClients.size();
+        }
     }
 }
