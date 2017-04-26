d7787fa13a30c134b4de188ea0e17a91eeb29a9d
Terry Ellison <ellisonterry4@gmail.com>
2017-04-26 18:19:45 -0400

    added fragment_clent_list added RecyclerView to fragment_client_list and ClientListFragment

--- app/src/main/java/com/terellison/android/fitness2go/ClientListFragment.java
+++ app/src/main/java/com/terellison/android/fitness2go/ClientListFragment.java
@@ -1,6 +1,12 @@
 package com.terellison.android.fitness2go;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Student on 4/26/2017.
@@ -8,4 +14,16 @@ import android.support.v4.app.Fragment;
 
 public class ClientListFragment extends Fragment {
 
+    private RecyclerView mClientRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceStae) {
+        View view = inflater.inflate(R.layout.fragment_client_list, container, false);
+
+        mClientRecyclerView = (RecyclerView) view.findViewById(R.id.client_recycler_view);
+        mClientRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 }
